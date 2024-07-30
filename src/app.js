50cfd1b3f95b8b6878c3b0b4b34b81779be102f0
nazi <nazipaeez777@gmail.com>
2024-07-30 23:46:33 +0330

    Develope app.js

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+/*Cors Plicy*/
+
+/*Body Parser*/
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.json({ limit: "50mb" }));
+
+/*Static Files*/
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+/*Routes*/
+
+/*404 Error Handler*/
+app.use((req, res) => {
+  console.log("this path not found", req.path);
+  return res.status(404).json({ message: "this path not found" });
+});
+
+/*Global Error Eandler*/
+
+module.exports = app;
