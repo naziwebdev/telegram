d6451aa344305a8095d7db2ee1aa092db2616820
nazi <nazipaeez777@gmail.com>
2024-07-30 23:57:09 +0330

    Develope server

---
+++ src/server.js
@@ -0,0 +1,30 @@
+const app = require("./app");
+const mongoose = require("mongoose");
+const http = require("http");
+require("dotenv").config();
+
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URL);
+    console.log("db connected successfully");
+  } catch (error) {
+    console.log("connect to mongodb faild", error);
+    process.exit(1);
+  }
+};
+
+const startServer = () => {
+  const port = process.env.PORT || 4001;
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(port, () => {
+    console.log(`Server running on  port ${port}`);
+  });
+};
+
+const run = async () => {
+  await connectToDB();
+  startServer();
+};
+
+run();
