e8cea8ecbd53ce83023e4a4d3229560080c6db18
nazi <nazipaeez777@gmail.com>
2024-07-31 00:15:32 +0330

    Develope uploader

---
+++ src/middlewares/multer.js
@@ -0,0 +1,41 @@
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+
+exports.multerStorage = (destination, allowTypes = /png|jpg|jpeg|webp/) => {
+  if (!fs.existsSync(destination)) {
+    fs.mkdirSync(destination, { recursive: true });
+  }
+
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, destination);
+    },
+
+    filename: (req, file, cd) => {
+      const unique = new Date.now() * Math.floor(Math.random() * 999999);
+
+      const ext = path.extname(file.originalname);
+
+      cb(null, `${unique}${ext}`);
+    },
+  });
+
+  const fileFilter = (req, file, cb) => {
+    if (allowTypes.test(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error("the format file is invalid"));
+    }
+  };
+
+  const uploader = multer({
+    storage,
+    limits: {
+      fileSize: 512_000_000,
+    },
+    fileFilter,
+  });
+
+  return uploader;
+};
