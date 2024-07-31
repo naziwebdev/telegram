bb8b7db3a1e1fe30487608217a56495dd3292537
nazi <nazipaeez777@gmail.com>
2024-07-31 18:47:06 +0330

    Develope getAll namespaces controller

--- src/modules/namespace/namespace.controller.js
+++ src/modules/namespace/namespace.controller.js
@@ -1,5 +1,9 @@
+const namespaceModel = require("../../models/Chat");
+
 exports.getAll = async (req, res, next) => {
   try {
+    const namespaces = await namespaceModel.find({}, { rooms: 0 }).lean();
+    return res.status(200).json(namespaces);
   } catch (error) {
     next(error);
   }
